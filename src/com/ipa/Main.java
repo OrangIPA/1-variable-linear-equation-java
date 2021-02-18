eeda4e0e9b3b2a2e2a69742392a3faafb2654edd
Bahana Alana Khansa <alanabahana102@gmail.com>
2021-02-18 15:08:12 +0700

    first commit

---
+++ src/com/ipa/Main.java
@@ -0,0 +1,15 @@
+package com.ipa;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("===== Root of 1 Variable Linear Equation =====");
+        System.out.println("note: only accept 1 variable linear equation, if contain non linear or more than variable, this program won't work");
+        System.out.print("Masukkan Persamaan: ");
+        String inputPersamaan = input.nextLine();
+        
+    }
+}
